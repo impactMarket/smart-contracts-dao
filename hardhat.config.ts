4e5439470a2249c47dad9d8b40e287a3220d6091
Aaron Boyd <aaronmboyd@gmail.com>
2021-07-22 12:46:38 +0200

    [CHORE] - Add hardhat-deploy dependencies and configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,9 @@ import { task } from "hardhat/config";
 import "solidity-coverage";
 import "hardhat-docgen";
 import "@nomiclabs/hardhat-waffle";
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -13,16 +13,66 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const CELO_DERIVATION_PATH = "m/44'/52752'/0'/0/";
+function getMnemonic(network:string) : string {
+	require("dotenv").config({ path: `.env.${network}` });
+	return process.env.MNEMONIC || '';
+};
 
 export default {
-  solidity: "0.8.5",
   networks: {
     hardhat: {
-      allowUnlimitedContractSize: true
+      hardfork: "istanbul",
+      allowUnlimitedContractSize: true,
+      gasPrice: "auto",
+			gas: 13000000,
+    },
+    alfajores: {
+      chainId: 44787,
+      url: "https://alfajores-forno.celo-testnet.org",
+      hardfork: "istanbul",
+      accounts: {
+        mnemonic: getMnemonic("alfajores"),
+        path: CELO_DERIVATION_PATH,
+      },
+      allowUnlimitedContractSize: true,
+      gas: "auto",
+      gasPrice: "auto",
+      blockGasLimit: 13000000,
+    },
+    baklava: {
+      chainId: 62320,
+      url: "https://baklava-forno.celo-testnet.org",
+      hardfork: "istanbul",
+      accounts: {
+        mnemonic: getMnemonic("baklava"),
+        path: CELO_DERIVATION_PATH,
+      },
+      allowUnlimitedContractSize: true,
+      gasPrice: "auto",
+			gas: "auto",
+      blockGasLimit: 13000000,
+    },
+    mainnet: {
+      chainId: 42220,
+      url: "https://forno.celo.org",
+      hardfork: "istanbul",
+      accounts: {
+        mnemonic: getMnemonic("mainnet"),
+        path: CELO_DERIVATION_PATH,
+      },
+      allowUnlimitedContractSize: true,
+      gasPrice: "auto",
+			gas: "auto",
+      blockGasLimit: 13000000,  
     }
   },
+  solidity: "0.8.5",
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  }
   // docgen: {
   //   path: './docs',
   //   clear: true,
