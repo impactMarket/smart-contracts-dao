17b0075c316bb513ff5d0286851328b4188f9632
Eduard Dumea <eduard@keyko.io>
2021-07-27 13:32:14 +0300

    work in progress

--- contracts/mocks/IPCTDelegatorMock.sol
+++ contracts/mocks/IPCTDelegatorMock.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.5;
 
-import "@nomspace/nomspace/contracts/interfaces/INom.sol";
 import "../governor/IPCTDelegator.sol";
 
 contract IPCTDelegatorMock is IPCTDelegator {
@@ -15,9 +14,5 @@ contract IPCTDelegatorMock is IPCTDelegator {
     uint votingDelay_,
     uint proposalThreshold_
   ) IPCTDelegator(timelock_, token_, releaseToken_, admin_, implementation_, votingPeriod_, votingDelay_, proposalThreshold_) {}
-
-  function resolve(bytes32 addr) public pure override returns (address) {
-    return address(uint160(uint256(addr) >> (12 * 8)));
-  }
 }
 
