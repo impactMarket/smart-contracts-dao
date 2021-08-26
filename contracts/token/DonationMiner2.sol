6c3a287a10dc51f04ab20ca7b937345780cdc47d
Eduard Dumea <eduard@keyko.io>
2021-08-26 09:17:07 +0300

    added more tests and comments

--- contracts/token/DonationMiner2.sol
+++ contracts/token/DonationMiner2.sol
@@ -24,23 +24,8 @@ contract DonationMiner2 is Ownable, Pausable, ReentrancyGuard {
     uint256 public blocksInRewardPeriod;
     uint256 public startingRewardBlock;
 
-    //    struct RewardPeriodUserInfo {
-    //        uint256 donationAmount; // How many cUSD tokens the user has provided this rewardPeriod.
-    //        uint256 claimedDonationAmount; // Amount of cUSD tokens  for which the reward was requested by user
-    //    }
-    //
-    //    struct RewardPeriodReward {
-    //        uint256 rewardPerBlock; // Reward tokens created per block.
-    //        uint256 startBlock; // The block number at which reward distribution starts.
-    //        uint256 endBlock; // The block number at which reward distribution ends.
-    //        uint256 donations; // Total of donations for this rewardPeriod.
-    //        uint256 claimedDonations; // Amount of cUSD tokens  for which the reward was requested by all users
-    //        uint256 ipctClaimed; // Amount of cUSD tokens  for which the reward was requested by all users
-    //        mapping(address => RewardPeriodUserInfo) rewardPeriodUsers;
-    //    }
-    //
-    //    mapping(uint256 => RewardPeriodReward) private rewards;
-    //    mapping(address => uint256) private userLastClaimedRewardPeriod;
+    uint256 public unclaimedReward;
+    uint256 public lastUnclaimedBlock;
 
     struct Donation {
         address donor;
@@ -52,9 +37,9 @@ contract DonationMiner2 is Ownable, Pausable, ReentrancyGuard {
     uint256 donationsCount;
 
     struct Claim {
+        address donor;
         uint256 blockNumber;
-        uint256 totalReward;
-        uint256 claimedReward;
+        uint256 reward;
     }
     mapping(uint256 => Claim) private claims;
     uint256 claimsCount;
@@ -98,10 +83,12 @@ contract DonationMiner2 is Ownable, Pausable, ReentrancyGuard {
         treasuryAddress = _treasuryAddress;
         startingRewardBlock = _startingRewardPeriod;
         blocksInRewardPeriod = _blocksInRewardPeriod;
+
+        lastUnclaimedBlock = _startingRewardPeriod;
     }
 
-    function _getRewardPerBlock(uint256 _blockNumber) internal view returns (uint256) {
-        return 100e18;
+    function _rewardToAdd(uint256 _blockNUmber) internal view returns (uint256) {
+        return (_blockNUmber - lastUnclaimedBlock) * 100e18;
     }
 
     /**
@@ -131,6 +118,36 @@ contract DonationMiner2 is Ownable, Pausable, ReentrancyGuard {
      * @dev Calculate and claim all pending rewards for a user
      */
     function claimRewards() external whenNotPaused whenStarted nonReentrant {
+        (uint256 userDonations, uint256 totalDonations) = calculateUserReward(block.number);
+
+        if (userDonations == 0) {
+            return;
+        }
+
+        unclaimedReward += _rewardToAdd(block.number);
+        lastUnclaimedBlock = block.number;
+        uint256 claimAmount;
+        claimAmount = (unclaimedReward * userDonations) / totalDonations;
+
+        unclaimedReward -= claimAmount;
+
+        claimsCount++;
+        Claim storage claim = claims[claimsCount];
+        claim.blockNumber = block.number;
+        claim.reward = claimAmount;
+        claim.donor = msg.sender;
+
+        if (claimAmount > 0) {
+            IPCT.safeTransfer(msg.sender, claimAmount);
+            emit RewardClaim(msg.sender, claimAmount);
+        }
+    }
+
+    function getUnclaimedReward(uint256 _blockNumber) public view returns (uint256) {
+        return unclaimedReward + _rewardToAdd(_blockNumber);
+    }
+
+    function calculateUserReward(uint256 _blockNumber) public view returns (uint256, uint256) {
         uint256 userDonations;
         uint256 totalDonations;
         uint256 iterator = donationsCount;
@@ -141,49 +158,13 @@ contract DonationMiner2 is Ownable, Pausable, ReentrancyGuard {
         while (donations[iterator].blockNumber >= startingBlock) {
             if (donations[iterator].isClaimed == false && donations[iterator].donor == msg.sender) {
                 userDonations = userDonations + donations[iterator].amount;
-                donations[iterator].isClaimed = true;
+                //                donations[iterator].isClaimed = true;     !!!!!!!!!!!!must to uncomment this one
             }
 
             totalDonations = totalDonations + donations[iterator].amount;
             iterator--;
         }
 
-        if (userDonations == 0) {
-            return;
-        }
-
-        claimsCount++;
-        Claim storage claim = claims[claimsCount];
-        claim.blockNumber = block.number;
-        claim.totalReward = _getRewardPerBlock(block.number);
-
-        uint256 lastRewardedBlock = block.number;
-        uint256 claimAmount;
-        uint256 blockClaim;
-        iterator = claimsCount - 1;
-        while (claims[iterator].blockNumber > startingBlock) {
-            blockClaim =
-                ((claims[iterator + 1].totalReward - claims[iterator + 1].claimedReward) *
-                    userDonations) /
-                totalDonations;
-            claims[iterator + 1].claimedReward += blockClaim;
-            claimAmount +=
-                blockClaim *
-                (claims[iterator + 1].blockNumber - claims[iterator].blockNumber);
-            iterator--;
-        }
-
-        blockClaim =
-            ((claims[iterator + 1].totalReward - claims[iterator + 1].claimedReward) *
-                userDonations) /
-            totalDonations;
-        claims[iterator + 1].claimedReward += blockClaim;
-        claimAmount += blockClaim * (claims[iterator + 1].blockNumber - startingBlock);
-
-        if (claimAmount > 0) {
-            IPCT.safeTransfer(msg.sender, claimAmount);
-        }
-
-        emit RewardClaim(msg.sender, claimAmount);
+        return (userDonations, totalDonations);
     }
 }
