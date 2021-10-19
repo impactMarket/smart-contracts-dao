3a81f1cf1e6f4226cd094e20a407541106d75214
Eduard Dumea <eduard@keyko.io>
2021-10-19 10:39:12 +0300

    some refactor

--- contracts/community/interfaces/CommunityAdminStorageV1.sol
+++ contracts/community/interfaces/CommunityAdminStorageV1.sol
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.5;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import "./ICommunityAdmin.sol";
 import "../../token/interfaces/ITreasury.sol";
-
-import "hardhat/console.sol";
 
 /**
  * @title Storage for CommunityAdmin
