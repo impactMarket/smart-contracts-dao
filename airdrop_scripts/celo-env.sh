5cec9f406245be4d0c3a0a1f2bbb927bc3ba072b
Eduard Dumea <eduard@keyko.io>
2021-12-28 15:45:20 +0200

    added celo-env.sh

---
+++ airdrop_scripts/celo-env.sh
@@ -0,0 +1,7 @@
+export IMPACT_MARKET_ADDRESS=0xe55C3eb4a04F93c3302A5d8058348157561BF5ca
+export COMMUNITY_FACTORY_ADDRESS=0xF3ba2c917b01627fb90673Aae0E170EE767Af8b6
+export WEB3_NETWORK=https://celo-mainnet--rpc.datahub.figment.io/apikey/<apikey>
+
+export IMARKET_ABI=./airdrop_scripts/abi/ImpactMarket.json
+export CFACTORY_ABI=./airdrop_scripts/abi/CommunityFactory.json
+export COMMUNITY_ABI=./airdrop_scripts/abi/Community.json
