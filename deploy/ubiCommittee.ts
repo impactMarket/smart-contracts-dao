b962d1c1c298218bd0a05e8ca52ccdcef4f3c810
Bernardo Vieira <bernardo@impactmarket.com>
2022-02-25 23:33:50 +0000

    add deploy scripts

---
+++ deploy/ubiCommittee.ts
@@ -0,0 +1,36 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+	// @ts-ignore
+	const { deployments, getNamedAccounts, ethers } = hre;
+	const { deploy } = deployments;
+	const { deployer } = await getNamedAccounts();
+	const committeeMember = deployer; // not recomeded to prod
+
+	const ImpactProxyAdminContract = await deployments.get("ImpactProxyAdmin");
+
+	const implementationResult = await deploy("UBICommitteeImplementation", {
+		from: deployer,
+		log: true,
+	});
+
+	const proxyResult = await deploy("UBICommitteeProxy", {
+		from: deployer,
+		args: [implementationResult.address, ImpactProxyAdminContract.address],
+		log: true,
+	});
+
+	const ubiCommittee = await ethers.getContractAt(
+		"UBICommitteeImplementation",
+		proxyResult.address
+	);
+
+	const communityAdminProxy = await deployments.get("CommunityAdminProxy");
+
+	await ubiCommittee.initialize(1, communityAdminProxy.address, [committeeMember]);
+};
+
+func.dependencies = ["ImpactProxyAdminProd", "CommunityProd"];
+func.tags = ["UBICommitteeProd", "Prod"];
+export default func;
