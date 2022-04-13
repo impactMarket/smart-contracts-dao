d2789382aeacfb288a5e79a59a1baf43487cd399
Eduard Dumea <eduard@keyko.io>
2022-04-13 15:33:30 +0300

    added community upgrade script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,10 +13,14 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   }
 });
 
-const CELO_DERIVATION_PATH = "m/44'/52752'/0'/0/";
 function getMnemonic(network:string) : string {
 	require("dotenv").config({ path: `.env.${network}` });
 	return process.env.MNEMONIC || '';
+};
+
+function getPrivateKey(network:string) : string {
+  require("dotenv").config({ path: `.env.${network}` });
+  return process.env.PRIVATE_KEY || '';
 };
 
 export default {
@@ -32,36 +36,18 @@ export default {
       chainId: 44787,
       url: "https://alfajores-forno.celo-testnet.org",
       hardfork: "istanbul",
-      accounts: {
-        mnemonic: getMnemonic("alfajores"),
-        path: CELO_DERIVATION_PATH,
-      },
+      accounts: [getPrivateKey("alfajores")],
       allowUnlimitedContractSize: true,
       gas: "auto",
       gasPrice: "auto",
-      blockGasLimit: 13000000,
-    },
-    baklava: {
-      chainId: 62320,
-      url: "https://baklava-forno.celo-testnet.org",
-      hardfork: "istanbul",
-      accounts: {
-        mnemonic: getMnemonic("baklava"),
-        path: CELO_DERIVATION_PATH,
-      },
-      allowUnlimitedContractSize: true,
-      gasPrice: "auto",
-			gas: "auto",
       blockGasLimit: 13000000,
     },
     mainnet: {
       chainId: 42220,
       url: "https://forno.celo.org",
       hardfork: "istanbul",
-      accounts: {
-        mnemonic: getMnemonic("mainnet"),
-        path: CELO_DERIVATION_PATH,
-      },
+      accounts: [getPrivateKey("mainnet")],
+
       allowUnlimitedContractSize: true,
       gasPrice: "auto",
 			gas: "auto",
