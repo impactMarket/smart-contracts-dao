ff0ef3483fe476c9e0464be9b3b1b71d2db960b6
Eduard Dumea <eduard@keyko.io>
2022-07-06 17:03:57 +0300

    added holder info script

--- deploy/prod/laterDeployment/release.upgradeImplementation.ts
+++ deploy/prod/laterDeployment/release.upgradeImplementation.ts
@@ -8,10 +8,18 @@ import * as ethersTypes from "ethers";
 const { deploy } = deployments;
 let deployer: SignerWithAddress;
 
-const governanceDelegatorAddress = "0x5c27e2600a3eDEF53DE0Ec32F01efCF145419eDF";
-const proxyAdminAddress = "0x79f9ca5f1A01e1768b9C24AD37FF63A0199E3Fe5";
+// mainnet
+const governanceDelegatorAddress = "0x8f8BB984e652Cb8D0aa7C9D6712Ec2020EB1BAb4";
+const proxyAdminAddress = "0xFC641CE792c242EACcD545B7bee2028f187f61EC";
+const targetAddress = "0xd61c407c3A00dFD8C355973f7a14c55ebaFDf6F9";
 
-const proxyAddress = "0x2Bdd85857eDd9A4fAA72b663536189e38D8E3C71";
+
+// //alfajores
+// const governanceDelegatorAddress = "0x5c27e2600a3eDEF53DE0Ec32F01efCF145419eDF";
+// const proxyAdminAddress = "0x79f9ca5f1A01e1768b9C24AD37FF63A0199E3Fe5";
+// const targetAddress = "0x1c33D75bcE52132c7a0e220c1C338B9db7cf3f3A";
+
+const contractName = 'CommunityAdminImplementation';
 
 let newImplementationAddress: string;
 
@@ -36,7 +44,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 async function deployNewImplementation() {
 	console.log("Deploying new contract for donation miner");
 	newImplementationAddress = (
-		await deploy("StakingImplementation", {
+		await deploy(contractName, {
 			from: deployer.address,
 			args: [],
 			log: true,
@@ -54,16 +62,16 @@ async function createUpgradeImplementation() {
 		deployer,
 		[
 			proxyAdminAddress,
-			proxyAddress
+			targetAddress
 		],
 		[0, 0],
 		[
 			"upgrade(address,address)",
-			"claimAmount(address)"
+			"communities(address)"   //this method is called only to check if the new implementation is correct
 		],
 		[["address", "address"], ["address"]],
 		[
-			[proxyAddress, newImplementationAddress],
+			[targetAddress, newImplementationAddress],
 			[deployer.address]
 		],
 		'Upgrade implementation'
